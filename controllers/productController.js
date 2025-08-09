58d13d2525856b85642c061b192aaeb59d38b65e
RachelKennedy09 <rachelkennedy@live.ca>
2025-08-09 13:36:33 -0600

    Added post /api/products with basic vaildation

--- controllers/productController.js
+++ controllers/productController.js
@@ -19,3 +19,47 @@ export async function getProducts(req, res) {
     res.status(500).json({ error: err.message });
   }
 }
+
+//CREATE - POST /api/products
+// Requests JSON body 
+// Error and success handling messages
+
+export async function createProduct(req, res) {
+  try {
+    //1. pull fields from the JSON body
+    const { name, category, price, quantity } = req.body;
+
+    //2. validation (null catches both undefined and null so - is valid)
+    if (!name || !category || price == null || quantity == null) {
+      return res.status(400).json({
+        error: "name, category, price, and quantity are required",
+      });
+    }
+
+    //Type checks for user clarity
+    if (typeof name !== "string" || typeof category !== "string") {
+      return res
+        .status(400)
+        .json({ error: "name and category must be strings" });
+    }
+    if (typeof price !== "number" || typeof quantity !== "number") {
+      return res
+        .status(400)
+        .json({ error: "price and quantity must be numbers" });
+    }
+    if (price < 0 || quantity < 0) {
+      return res
+        .status(400)
+        .json({ error: "price and quantity cannot be negative" });
+    }
+
+    // 3. create the product in MongoDB
+    const created = await Product.create({ name, category, price, quantity });
+
+    //4. respond with 201 create and the new product
+    return res.status(201).json(created);
+  } catch (err) {
+    //5. if anything unexpected happens (e.g. DB down), return 500 (server error)
+    return res.status(500).json({ error: err.message });
+  }
+}
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,10 +5,14 @@
 
 import { Router } from "express";
 import { getProducts } from "../controllers/productController.js";
+import { createProduct } from "../controllers/productController.js";
 
 const router = Router();
 
-// READ ALL
+// READ ALL - send a get request to /api/products with JSON body
 router.get("/", getProducts); //GET /api/products
+
+// CREATE - send a post request to /api/products with JSON body
+router.post("/", createProduct); //POST /api/product
 
 export default router;
